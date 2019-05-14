ec9d9c698d9c108dbb9bf4ed3f539e56117aafea
simons <licb@mogulinker.com>
2019-05-14 17:40:21 +0800

    v2支持遍历文件夹

--- src/index.js
+++ src/index.js
@@ -14,13 +14,53 @@ Object.keys(tempObj).forEach(value => {
   mapObj[tempObj[value]] = value.toLowerCase();
 });
 
-fs.readdir(filePath, "utf-8", function(err, data) {
-  data.forEach(function(item, index) {
-    if (/.less/.test(item)) {
-      replaceColor(filePath + "/" + item,filePath);
-    }
+
+/**
+ * 文件遍历方法
+ * @param filePath 需要遍历的文件路径
+ */
+function fileDisplay(filePath){
+  //根据文件路径读取文件，返回文件列表
+  fs.readdir(filePath,function(err,files){
+      if(err){
+          console.warn(err)
+      }else{
+          //遍历读取到的文件列表
+          files.forEach(function(filename){
+              //获取当前文件的绝对路径
+              const filedir = path.join(filePath,filename);
+              //根据文件路径获取文件信息，返回一个fs.Stats对象
+              fs.stat(filedir,function(eror,stats){
+                  if(eror){
+                      console.warn('获取文件stats失败');
+                  }else{
+                      const isFile = stats.isFile();//是文件
+                      const isDir = stats.isDirectory();//是文件夹
+                      if(isFile){
+                          wrap(filedir,filePath)
+                      }
+                      if(isDir){
+                          fileDisplay(filedir);//递归，如果是文件夹，就继续遍历该文件夹下面的文件
+                      }
+                  }
+              })
+          });
+      }
   });
-});
+}
+
+function wrap(filePath,baseUrl){
+  if (/(.less)$/.test(filePath)) {
+    replaceColor(filePath,baseUrl);
+  }
+}
+// fs.readdir(filePath, "utf-8", function(err, data) {
+//   data.forEach(function(item, index) {
+//     if (/.less/.test(item)) {
+//       replaceColor(filePath + "/" + item,filePath);
+//     }
+//   });
+// });
 
 function replaceColor(url,baseUrl) {
   console.log(url);
@@ -45,3 +85,4 @@ function replaceColor(url,baseUrl) {
 }
 
 // console.log(Object.keys(mapObj));
+fileDisplay(filePath)
