b527f35f702b447e0cfc7becebc5b467654e3462
simons <licb@mogulinker.com>
2019-05-14 17:43:48 +0800

    直接更改less文件 完成

--- src/index.js
+++ src/index.js
@@ -14,44 +14,43 @@ Object.keys(tempObj).forEach(value => {
   mapObj[tempObj[value]] = value.toLowerCase();
 });
 
-
 /**
  * 文件遍历方法
  * @param filePath 需要遍历的文件路径
  */
-function fileDisplay(filePath){
+function fileDisplay(filePath) {
   //根据文件路径读取文件，返回文件列表
-  fs.readdir(filePath,function(err,files){
-      if(err){
-          console.warn(err)
-      }else{
-          //遍历读取到的文件列表
-          files.forEach(function(filename){
-              //获取当前文件的绝对路径
-              const filedir = path.join(filePath,filename);
-              //根据文件路径获取文件信息，返回一个fs.Stats对象
-              fs.stat(filedir,function(eror,stats){
-                  if(eror){
-                      console.warn('获取文件stats失败');
-                  }else{
-                      const isFile = stats.isFile();//是文件
-                      const isDir = stats.isDirectory();//是文件夹
-                      if(isFile){
-                          wrap(filedir,filePath)
-                      }
-                      if(isDir){
-                          fileDisplay(filedir);//递归，如果是文件夹，就继续遍历该文件夹下面的文件
-                      }
-                  }
-              })
-          });
-      }
+  fs.readdir(filePath, function(err, files) {
+    if (err) {
+      console.warn(err);
+    } else {
+      //遍历读取到的文件列表
+      files.forEach(function(filename) {
+        //获取当前文件的绝对路径
+        const filedir = path.join(filePath, filename);
+        //根据文件路径获取文件信息，返回一个fs.Stats对象
+        fs.stat(filedir, function(eror, stats) {
+          if (eror) {
+            console.warn("获取文件stats失败");
+          } else {
+            const isFile = stats.isFile(); //是文件
+            const isDir = stats.isDirectory(); //是文件夹
+            if (isFile) {
+              wrap(filedir, filedir);
+            }
+            if (isDir) {
+              fileDisplay(filedir); //递归，如果是文件夹，就继续遍历该文件夹下面的文件
+            }
+          }
+        });
+      });
+    }
   });
 }
 
-function wrap(filePath,baseUrl){
+function wrap(filePath, baseUrl) {
   if (/(.less)$/.test(filePath)) {
-    replaceColor(filePath,baseUrl);
+    replaceColor(filePath, baseUrl);
   }
 }
 // fs.readdir(filePath, "utf-8", function(err, data) {
@@ -62,7 +61,7 @@ function wrap(filePath,baseUrl){
 //   });
 // });
 
-function replaceColor(url,baseUrl) {
+function replaceColor(url, filedir) {
   console.log(url);
   fs.readFile(url, "utf8", function(err, data) {
     if (err) {
@@ -78,11 +77,11 @@ function replaceColor(url,baseUrl) {
       }
       return value;
     });
-    fs.writeFile(baseUrl + "/result.less", result, "utf8", function(err) {
+    fs.writeFile(filedir, result, "utf8", function(err) {
       if (err) return console.log(err);
     });
   });
 }
 
 // console.log(Object.keys(mapObj));
-fileDisplay(filePath)
+fileDisplay(filePath);
