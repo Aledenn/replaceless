d0bdb2e6ef9e0df0e9212b5fa4ab564f1c113721
simons <licb@mogulinker.com>
2019-05-14 17:07:18 +0800

    v1支持单文件

--- src/index.js
+++ src/index.js
@@ -1,32 +1,46 @@
 // import {lessToJson} from 'less-to-json'
-const lessToJson = require('less-to-json');
-const fs = require('fs')
-const path = require('path')
+const lessToJson = require("less-to-json");
+const fs = require("fs");
+const path = require("path");
 
-function isJsonString(str) {
-  try {
-      if (typeof JSON.parse(str) == "object") {
-          return true;
-      }
-  } catch(e) {
-  }
-  return false;
-}
 // debugger
-const mapJson = lessToJson(path.dirname(__filename)+'/theme.less')
-// console.log(typeof mapJson);
-Object.keys(mapJson).forEach((value) => {
-  console.log(value)
-})
-// const mapObj = JSON.parse(mapJson)
+const tempObj = lessToJson(path.dirname(__filename) + "/theme.less"); // key - 颜色
+
+const mapObj = {};
+Object.keys(tempObj).forEach(value => {
+  // 把value转换为key
+  mapObj[tempObj[value]] = value.toLowerCase();
+});
+// const mapObj = JSON.parse(mapObj)
 // console.log(mapObj);
-// fs.readFile(path.dirname(__filename)+'/text.less','utf8',function(err,data){
-//   if(err){
-//     console.log(err);
-//   }
-//   const result = data
-// })
-
-
+fs.readFile(path.dirname(__filename) + "/text.less", "utf8", function(
+  err,
+  data
+) {
+  if (err) {
+    console.log(err);
+  }
+  const result = data.replace(/#{1}[a-zA-Z0-9]{5}[a-zA-Z0-9]/g, function(
+    value
+  ) {
+    // debugger
+    console.log(value);
+    // console.log(mapObj);
+    if (mapObj[value.toLowerCase()]) {
+      console.log(mapObj[value.toLowerCase()]);
+      return "@" + mapObj[value];
+    }
+    return value;
+  });
+  fs.writeFile(
+    path.dirname(__filename) + "/result.less",
+    result,
+    "utf8",
+    function(err) {
+      if (err) return console.log(err);
+    }
+  );
+  // console.log(result);
+});
 
 // console.log(Object.keys(mapObj));
